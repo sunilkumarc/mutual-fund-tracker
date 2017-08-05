d0baa9f01d9155220e77db65a76a7a639960a708
Sunil Kumar C <sunilkumar.c682@gmail.com>
2017-08-05 22:16:26 +0530

    Asked users to rate the extension

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 setTimeout(() => {
-    // Cookies.removeCookie('sync_feature_popup_showed');
-    // if (Cookies.get('sync_feature_popup_showed') === undefined) {
-    //     $('#popup').show();
-    //     Cookies.set('sync_feature_popup_showed', 1);
-    // }
+    if (Cookies.get('rating_popup_showed') === undefined) {
+        $('#popup').show();
+        Cookies.set('rating_popup_showed', 1);
+    }
 }, 1000);
