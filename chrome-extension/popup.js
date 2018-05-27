233e8f31d01e72d14e0830443c9eb10238108d7b
Sunil Kumar C <sunilkumar.c682@gmail.com>
2018-05-27 18:08:57 +0530

    Sent notification about Android application

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -2,6 +2,9 @@ setTimeout(() => {
     var mobile_client_popup = window.localStorage.getItem('details-page-communication');
     if (mobile_client_popup == null) {
         $('#popup').show();
-        window.localStorage.setItem('details-page-communication', 1);
+        window.localStorage.setItem('details-page-communication', 4);
+    } else if (mobile_client_popup > 0) {
+        $('#popup').show();
+        window.localStorage.setItem('details-page-communication', mobile_client_popup-1);
     }
 }, 1000);
